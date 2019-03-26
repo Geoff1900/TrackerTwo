094e7e8286032bf8b5d50495ca2ff4ff2b626d32
Geoff Norton <Geoff.Norton@wales.nhs.uk>
2019-03-26 12:13:23 +0000

    Created Licence, Controller, Models and View

---
+++ TrackerTwo/Models/LicenceItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrackerTwo.Models
+{
+    public class LicenceItem
+    {
+        public Guid Id { get; set; }
+
+        //TODO Move DataAnnotations to EntityType Configuration
+        [Required]
+        public string User { get; set; }
+        [Required]
+        public string Key { get; set; }
+    }
+}
---
+++ TrackerTwo/Models/LicenceViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrackerTwo.Models
+{
+    public class LicenceViewModel
+    {
+        public IEnumerable<LicenceItem> items { get; set; }
+    }
+}
