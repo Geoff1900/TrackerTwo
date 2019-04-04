ebc30cce65ad2d723052b6a2217e8d4992a82339
Geoff Norton <Geoff.Norton@wales.nhs.uk>
2019-04-04 14:43:28 +0000

    Added Controller, ViewModel and View for Manager Users functionality but not wired up services or seeded data

--- TrackerTwo/Controllers/ManageUsersController.cs
+++ TrackerTwo/Controllers/ManageUsersController.cs
@@ -2,15 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TrackerTwo.Models;
 
 namespace TrackerTwo.Controllers
 {
+
+    [Authorize(Roles = Constants.AdministratorRole)]
     public class ManageUsersController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<IdentityUser> _userManager;
+        public ManageUsersController(UserManager<IdentityUser> userManager)
         {
-            return View();
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var administrators = (await _userManager.GetUsersInRoleAsync(Constants.AdministratorRole)).ToList();
+            var everyone = await _userManager.Users.ToListAsync();
+
+            var model = new UsersViewModel() { Administrators = administrators, Everyone = everyone };
+            return View(model);
         }
     }
 }
---
+++ TrackerTwo/Models/UsersViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrackerTwo.Models
+{
+    public class UsersViewModel
+    {
+        public IEnumerable<IdentityUser> Administrators { get; set; }
+        public IEnumerable<IdentityUser> Everyone { get; set; }
+
+    }
+}
