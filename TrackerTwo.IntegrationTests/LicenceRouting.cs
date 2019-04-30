c839bd79036bd9b44e44a9f73b806c48a0dffb1d
Geoff Norton <Geoff.Norton@wales.nhs.uk>
2019-04-30 08:59:36 +0000

    Added Integration test using .Net Core 2.2's test Server functionality

---
+++ TrackerTwo.IntegrationTests/LicenceRouting.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TrackerTwo.IntegrationTests
+{
+    public class LicenceRouting : IClassFixture<TestFixture>
+    {
+        private readonly HttpClient _client;
+
+        public LicenceRouting(TestFixture fixture)
+        {
+            _client = fixture.Client;
+        }
+
+        [Fact]
+        public async Task LicenceControllerIndex_AnonymousGet_RedirectChallenge()
+        {
+            //arrange
+            var request = new HttpRequestMessage(HttpMethod.Get, "/Licence");
+
+            //act
+            var response = await _client.SendAsync(request);
+
+            //assert
+            Assert.Equal(HttpStatusCode.Redirect, response.StatusCode);
+
+            Assert.Equal("http://localhost:8888/Identity/Account/Login?ReturnUrl=%2FLicence",response.Headers.Location.ToString());
+
+        }
+
+    }
+}
